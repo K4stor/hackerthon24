1e6b256c303d8175ae7a0f1530cd8f75f57380f6
Carsten Przyluczky <carsten.przyluczky@check24.de>
2024-05-03 16:14:04 +0200

    added bluetooth support

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <Servo.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Servo speedServo;
+Servo directionServo;
+
+SoftwareSerial BTSerial (2,3);
+
+int speedPin = 10;
+int directionPin = 9;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  BTSerial.begin(9600);
+
+  speedServo.attach(speedPin);
+  directionServo.attach(directionPin);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if(BTSerial.available()) {
+
+    char value = BTSerial.read();
+
+    char speed = 0b00001111 & value;
+    char direction = (0b11110000 & value) >> 4;
+
+    unsigned char mappedDirection = map(direction, 0, 127, 40, 140); 
+    unsigned char mappedSpeed = map(speed, 0, 127, 0, 180); 
+    
+    speedServo.write(mappedSpeed);
+    directionServo.write(mappedDirection);
+    delay(15); 
+  }
 }
